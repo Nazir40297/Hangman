5843020e4679ff7d76d1cfc978e601ff532bd0ee
Nazir40297 <102291490+Nazir40297@users.noreply.github.com>
2023-03-20 17:14:27 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -4,3 +4,7 @@ word_list = ["pomegranate", "kiwi", "apple", "pineapple", "orange"]
 word = random.choice(word_list)
 print(word)
 guess = input("Enter a single letter...\n")
+if len(guess) == 1:
+  print("Good guess!.")
+else:
+  print("Oops! That is not a valid input.")
