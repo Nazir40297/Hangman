d0d359be0faf12b971a7626b54c847a84868fcfc
Nazir40297 <102291490+Nazir40297@users.noreply.github.com>
2023-03-20 21:58:22 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -5,6 +5,6 @@ class Hangman:
     self.word = word
     self.word_guessed = word_guessed
     self.num_letters = num_letters
-    self.num_lives = 5
+    self.num_lives = num_lives
     self.word_list = word_list
-    self.list_of_guesses = []
+    self.list_of_guesses = list_of_guesses
