caa3e004b82089bd467afd009568583b7648be40
Nazir40297 <102291490+Nazir40297@users.noreply.github.com>
2023-03-20 20:54:18 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,13 +1,20 @@
-while True:
-  guess = input("Guess a letter...\n")
-  if len(guess) == 1 and guess.isalpha():
+def ask_for_input():
+  while True:
+    guess = input("Guess a letter...\n")
+    if len(guess) == 1 and guess.isalpha():
     break
   else:
     print("Invalid letter. Please, enter a single alphabetical character.")
+  check_guess(guess)
 
-word = "pineapple"  
   
-if guess in word:
-  print(f"Good guess! {guess} is in the word.")
-else:
-  print(f"Sorry, {guess} is not in the word. Try again.")
+
+def check_guess(guess):
+  guess.lower()
+  word = "pineapple"  
+  if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+  else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+  
+ask_for_input()
