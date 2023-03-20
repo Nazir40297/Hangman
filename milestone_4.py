54f55a007a359f191b8323d321c86c4d66ec3075
Nazir40297 <102291490+Nazir40297@users.noreply.github.com>
2023-03-20 22:22:48 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -2,9 +2,9 @@ import random
 
 class Hangman:
   def __init__(self, word_list, num_lives = 5):
-    self.word = word
-    self.word_guessed = ['_'] * len(word)
-    self.num_letters = len(set(word))
+    self.word = random.choice(word_list)
+    self.word_guessed = ['_'] * len(self.word)
+    self.num_letters = len(set(self.word))
     self.num_lives = num_lives
     self.word_list = word_list
-    self.list_of_guesses = list_of_guesses
+    self.list_of_guesses = []
