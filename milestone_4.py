5d064ccceb8bb1d1068a7295a501e64c8d868a52
Nazir40297 <102291490+Nazir40297@users.noreply.github.com>
2023-03-21 22:34:25 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -8,3 +8,21 @@ class Hangman:
     self.num_lives = num_lives
     self.word_list = word_list
     self.list_of_guesses = []
+  
+  def check_guess(self, guess):
+    guess = guess.lower()
+    if guess in self.word:
+      print(f"Good guess! {guess} is in the word.")
+  
+  def ask_for_input(self):
+    while True:
+      guess = input("Guess a letter...\n")
+      if guess.isalpha() == False:
+        print("Invalid letter. Please, enter a single alphabetical letter.")
+      elif guess in self.list_of_guesses:
+        print("You already tried that letter!")
+      else:
+        check_guess(guess)
+        self.list_of_guesses.append(guess)
+  
+        
