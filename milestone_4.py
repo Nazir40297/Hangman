0782eefe835793a86796528e44221eee5d05fc2f
Nazir40297 <102291490+Nazir40297@users.noreply.github.com>
2023-03-22 00:24:04 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -19,8 +19,9 @@ class Hangman:
       self.num_letters -= 1
     else:
         self.num_lives -= 1
+        num_lives = self.num_lives
         print(f"Sorry, {guess} is not in the word.")
-        print(f"You have {self.num_lives} left.")
+        print(f"You have {num_lives} left.")
   
   def ask_for_input(self):
     while True:
